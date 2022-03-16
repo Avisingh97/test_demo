62e196275aea7b8a4ac919aed7d4eb28ba562fb0
aviral <singhaviral3@gmail.com>
2022-03-16 20:33:48 +0530

    firstcomment

---
+++ module_usecase/variable.tf
@@ -0,0 +1,24 @@
+variable "region" {
+    type = string
+  
+}
+variable "profile" {
+    type = string
+  
+ }
+variable "bucket_name" {
+  type = string
+}
+
+/* variable "iam_user" {
+  type = string
+} */
+
+
+/* variable "instance" {
+  type = map(any)  
+}
+ */
+variable "s3_bucket" {
+  type = map(any)  
+}
